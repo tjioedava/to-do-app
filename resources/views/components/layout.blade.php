a4193f38d14a497c0bb291065dea3b3f18256f84
tjioedava <tjioedava@gmail.com>
2024-08-13 23:05:58 +0700

    enhancing UI: adding icons, adjusting margins, better container sizing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
     @stack('styles')
+    @stack('scripts')
     <title>To Do App</title>
 </head>
 <body>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,10 @@
         <link rel="stylesheet" href="{{ asset('css/individual/index.css') }}">
     @endpush
 
+    @push('scripts')
+        <script src="https://kit.fontawesome.com/19ef3fcdaf.js" crossorigin="anonymous"></script>
+    @endpush
+
     <h1>To Do App</h1>
     <x-navigation/>
     <hr>
@@ -14,10 +18,12 @@
                 <h3 class="nodes">{{ $task->date }}</h4>
             </div>
             <div id="right">
-                <a class="nodes" href="{{ route('edit', $task->id) }}"><button>Edit</button></a>
+                <a class="nodes" href="{{ route('edit', $task->id) }}">
+                    <button><i class="fa-solid fa-pencil fa-lg"></i></button>
+                </a>
                 <form class="nodes" method="post" action="{{ route('destroy') }}">
                     @csrf
-                    <button name="button" value="{{ $task->id }}">Done</button>
+                    <button name="button" value="{{ $task->id }}"><i class="fa-solid fa-check fa-2xl"></i></button>
                 </form>
             </div>
         </div>
