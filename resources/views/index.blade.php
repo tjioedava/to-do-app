33124f9aac8aac9a83c820654e97843014d1606c
tjioedava <tjioedava@gmail.com>
2024-08-16 12:11:13 +0700

    refactoring index page tasks list classes and design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,18 @@
     <hr>
     @foreach ($tasks as $task)
         <div id="container">
-            <div id="left">
-                <h3 class="nodes"><a href="{{ route('show', $task->id) }}">{{ $task->description }}</a></h1>
-                <h3 class="nodes">{{ $task->date }}</h4>
+            <div id="non-controls-container">
+                <div id="description-container">
+                    <h3 class="nodes"><a href="{{ route('show', $task->id) }}">{{ $task->description }}</a></h1>
+                </div>
+                <div id="date-container">
+                    <h3 class="nodes">{{ $task->date }}</h3>    
+                </div>
+                <div id="category-container">
+                    <h3 class="nodes">{{ $task->category }}</h3>
+                </div>
             </div>
-            <div id="right">
+            <div id="controls-container">
                 <a class="nodes" href="{{ route('edit', $task->id) }}">
                     <button><i class="fa-solid fa-pencil fa-lg"></i></button>
                 </a>
